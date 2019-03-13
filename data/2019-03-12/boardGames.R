9c70035d2feb21d0e18ccf70429e5e0d9dc25950
Tanya D <tanyaberde@users.noreply.github.com>
2019-03-13 10:39:12 -0400

    Clean boardGames.R

--- data/2019-03-12/boardGames.R
+++ data/2019-03-12/boardGames.R
@@ -2,6 +2,7 @@ require(tidyverse)
 
 data <- read_csv("https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2019/2019-03-12/board_games.csv")
 
+# Take a peek at specs for Twilight Struggle
 tw <- data %>% 
   filter(name=="Twilight Struggle")
 
@@ -24,8 +25,6 @@ g <- ggplot(longDiceGames) +
   ) +
   geom_point(alpha = .5) +
   geom_smooth() +
-    # aes(x = playing_hours, y = average_rating),
-            # stat="smooth") +
   facet_wrap(~max_players) +
   guides(colour=F) +
   ggtitle("Correlation between game length and rating") +
